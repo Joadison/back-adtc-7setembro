3141445c17c68ce97c7345f6bd2b8e0389cb7df6
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-06-26 16:03:11 -0300

    Feate:gerarimagem

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const cors = require('cors');
 
 const generateImageRoutes = require('./routes/generateImage');
 const generateImageARoutes = require('./routes/generateImageA');
@@ -9,6 +9,11 @@ const { initializeBot } = require('./services/telegramBot');
 
 const app = express();
 const port = 3333;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', generateImageRoutes);
 app.use('/', generateImageARoutes);
