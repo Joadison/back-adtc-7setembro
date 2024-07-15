d9f70e131c595d0fc11f53c800e6a5bfe38211ee
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-07-15 13:11:01 -0300

    Feat: Reajust  Telegram

--- routes/generateImageA.js
+++ routes/generateImageA.js
@@ -2,16 +2,15 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
+const os = require('os');
 const { addTextToImageA } = require('../utils/agerarStory');
 
 router.get('/', async(req, res) => {
     const inputImagePath = `https://utfs.io/f/5e6e6114-76e5-4f6f-8894-e9a4fc4215f5-1g.png`;
-    const outputImagePath = path.join(__dirname, '..', 'public', 'Story', '1.png');
-    //const tempDir = os.tmpdir();
-    //const outputImagePath = path.join(tempDir, 'A.png');
+    const tempDir = os.tmpdir();
+    const outputImagePath = path.join(tempDir, '1.png');
     
     try {
-        await fs.mkdir(path.dirname(outputImagePath), { recursive: true });
         const imagePath = await addTextToImageA(inputImagePath, outputImagePath);
         const imageBuffer = await fs.readFile(outputImagePath);
         res.writeHead(200, {
