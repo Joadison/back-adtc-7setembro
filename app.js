3eec8fcabde3289f561fb951b701a4c2723ae931
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-07-15 12:34:23 -0300

    Feat: Reajust complet Telegram

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-require("dotenv").config();
-//const TelegramBot = require("node-telegram-bot-api");
-const TeleBot = require("telebot");
+require('dotenv').config();
+const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -9,19 +8,18 @@ const cors = require("cors");
 const generateImageRoutes = require("./routes/generateImage");
 const generateImageARoutes = require("./routes/generateImageA");
 
-//const token = process.env.TELEGRAM_TOKEN;
+const token = process.env.TELEGRAM_TOKEN;
 //const url = "https://back-adtc-7setembro.vercel.app";
-//const url = "http://localhost:3444"
+const url = "http://localhost:3444"
 
-/* if (!token) {
+if (!token) {
   throw new Error("TELEGRAM_TOKEN não está definido!");
 }
-const bot = new TelegramBot(token); */
 
-const bot = new TeleBot({ token: process.env.TELEGRAM_TOKEN });
+const bot = new TelegramBot(token);
 
 const app = express();
-const port = 3444;
+const port = 3444
 
 app.use(cors());
 
@@ -35,7 +33,14 @@ app.get("/", (req, res) => {
   res.send("API OK!");
 });
 
-bot.on(["/start"], (msg) => {
+app.post(`/bot/${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+bot.setWebHook(`${url}/bot${token}`);
+
+bot.on(['/start'], (msg) => {
   const chatId = msg.chat.id;
   const currentHour = new Date().getHours();
   let greeting;
@@ -47,39 +52,75 @@ bot.on(["/start"], (msg) => {
     greeting = "Boa noite";
   }
   const welcomeMessage = `${greeting}! Paz do Senhor! Em que posso ajudar você? Aqui estão algumas opções:
-  - Digite /jogo para jogar um jogo.
-  - Digite /oracao para enviar um pedido de oração.
-  - Digite /instagram para obter links para nossos perfis no Instagram.
-  - Digite /imagem para gerar uma imagem.`;
+- Digite /jogo para jogar um jogo.
+- Digite /oracao para enviar um pedido de oração.
+- Digite /instagram para obter links para nossos perfis no Instagram.
+- Digite /imagem para gerar uma imagem.`;
 
   bot.sendMessage(chatId, welcomeMessage);
+});
+
+bot.on(['/jogo'], (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Vamos jogar!!!");
+  bot.sendMessage(chatId, "https://joadison.github.io/QuizBiblico/");
+});
+
+bot.on(['/oracao'], (msg) => {
+  const chatId = msg.chat.id;
+  console.log(chatId);
+  bot.sendMessage(chatId, "Pode digitar, que já estaremos orando por você!");
+
+  bot.once("message", (msg) => {
+    const userChatId = msg.chat.id;
+    const oracaoText = msg.text;
+    const outroChatId = "1088237134";
+    const mensagem = `Alguém está pedindo oração:\n${oracaoText}`;
+    bot.sendMessage(outroChatId, mensagem);
+  });
+});
+
+bot.on(['/instagram'], (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Meu instagram é");
+  bot.sendMessage(chatId, "https://www.instagram.com/adtc.7setembro1");
+  bot.sendMessage(chatId, "E este é da nossa Juventude! ");
+  bot.sendMessage(chatId, "https://www.instagram.com/juventudetc7s1");
+});
+
+bot.on(['/imagem'], async (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Gerando imagem...");
+  try {
+    const response = await axios.get(`${url}/generateimageA`, { responseType: "arraybuffer" });
+    bot.sendPhoto(chatId, Buffer.from(response.data, "binary"));
+  } catch (error) {
+    bot.sendMessage(chatId, "Desculpe, ocorreu um erro ao processar a imagem.");
+  }
 });
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text.toString().toLowerCase();
 
-  if (messageText === "oi") {
-    bot.sendMessage(chatId, "Olá, tudo bem?");
+  if (messageText === 'oi') {
+    bot.sendMessage(chatId, 'Olá, tudo bem?');
   }
-  if (messageText === "paz do senhor!") {
-    bot.sendMessage(chatId, "A PAZ DO SENHOR! Que Deus seja conosco!");
-  } else if (messageText === "irmão, como você está?") {
-    bot.sendMessage(chatId, "Estou bem, graças a Deus. E você, como está?");
-  } else if (messageText === "Deus abençoe você!") {
-    bot.sendMessage(chatId, "Deus abençoe você também, irmão!");
+  if (messageText === 'paz do senhor!') {
+    bot.sendMessage(chatId, 'A PAZ DO SENHOR! Que Deus seja conosco!');
+  } else if (messageText === 'irmão, como você está?') {
+    bot.sendMessage(chatId, 'Estou bem, graças a Deus. E você, como está?');
+  } else if (messageText === 'Deus abençoe você!') {
+    bot.sendMessage(chatId, 'Deus abençoe você também, irmão!');
   } else {
-    bot.sendMessage(chatId, "Desculpe, não entendi. Como posso ajudar?");
+    bot.sendMessage(chatId, 'Desculpe, não entendi. Como posso ajudar?');
   }
 });
 
-app.post(`/bot/${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+console.log("Bot iniciado.");
 
 app.listen(port, () => {
   console.log(`Servidor está rodando em http://localhost:${port}`);
-});
+})
 
 module.exports = app;
